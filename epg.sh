430b3f460066bb238e294d273da375f5fa9e8940
Unicorn9x <unicorn9x@yahoo.com.vn>
2024-10-21 19:00:32 +0700

    Update epg.sh

--- epg.sh
+++ epg.sh
@@ -2,7 +2,7 @@
 . /etc/profile
 
 
-wget -O $HOME/xmltv.xmltv http://lichphatsong.xyz/schedule/epg.xml
+wget -O $HOME/xmltv/xmltv.xmltv http://lichphatsong.xyz/schedule/epg.xml
 /usr/bin/socat open:$HOME/xmltv/xmltv.xmltv UNIX-CONNECT:/docker_data/TVHeadend/epggrab/xmltv.sock
 
 #Unicorn9x - 2024 
